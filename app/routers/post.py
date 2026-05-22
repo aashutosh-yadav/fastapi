5b8194955ad281f8881171a806d32546ef5c4376
aashutosh-yadav <yadavashutoshh01@gmail.com>
2026-05-22 18:14:35 +0530

    a lot of things are added to posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -14,7 +14,7 @@ from sqlalchemy.util.deprecations import deprecated
 
 from app import oauth2
 
-from .. import models, schemas , oauth2
+from .. import models, oauth2, schemas
 from ..database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -23,26 +23,31 @@ router = APIRouter(prefix="/posts", tags=["Posts"])
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db) , current_user : int = Depends(oauth2.get_current_user)):
+def get_posts(
+    db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user) ,
+    limit : int = 10,
+    skip : int = 0,
+    search : Optional[str] = ""
+):
     # cursor.execute("SELECT * FROM posts")
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 
-
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def creat_post(post: schemas.PostCreate, db: Session = Depends(get_db) , current_user : int = Depends(oauth2.get_current_user)):
+def creat_post(
+    post: schemas.PostCreate,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
     # cursor.execute(
     #    "INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *",
     #    (post.title, post.content, post.published),
     # )
     # new_post = cursor.fetchone()
     # conn.commit()
-    print(current_user.email)
-    new_post = models.Post(  # -> this is for efficiency
-        **post.model_dump()
-    )
+    new_post = models.Post(owner_id=current_user.id, **post.model_dump())
     # new_post = models.Post(title=post.title , content=post.content , published=post.published) # create new post  -> this is slow
     db.add(new_post)  # add it to the database
     db.commit()  # commit the changes
@@ -57,8 +62,12 @@ def creat_post(post: schemas.PostCreate, db: Session = Depends(get_db) , current
 #         }
 
 
-@router.get("/{id}" , response_model=schemas.Post)  # path parameter
-def get_post(id: int, db: Session = Depends(get_db) , current_user : int = Depends(oauth2.get_current_user)):
+@router.get("/{id}", response_model=schemas.Post)  # path parameter
+def get_post(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
     # cursor.execute("SELECT * FROM posts WHERE id = %s", (id,))
     # post = cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id).first()
@@ -69,25 +78,35 @@ def get_post(id: int, db: Session = Depends(get_db) , current_user : int = Depen
         )
         # response.status_code = status.HTTP_404_NOT_FOUND
     # return {"message": f"post with id :{id} was not found "}
-    return {"post": post}
+    return post
 
 
 # delete method
-@router.delete("/{id}", status_code=status.HTTP_200_OK)
-def delete_post(id: int, db: Session = Depends(get_db) , current_user : int = Depends(oauth2.get_current_user)):
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
     # cursor.execute("DELETE FROM posts WHERE id = %s RETURNING *", (id,))
     # post = cursor.fetchone()
     # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    if not post:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found"
         )
+    if post.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="not authorized to preform requested action",
+        )
 
-    db.delete(post)
+    post_query.delete(synchronize_session=False)
     db.commit()
 
-    return {"message": "deleted sucessfully"}
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 #
@@ -110,21 +129,30 @@ def delete_post(id: int, db: Session = Depends(get_db) , current_user : int = De
 
 
 @router.put("/{id}")
-def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db) , current_user : int = Depends(oauth2.get_current_user)):
+def update_post(
+    id: int,
+    updated_post: schemas.PostCreate,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
 
-    existing_post = (
-        post_query.first()
-    )  # here existing_post post  is the post that already exist int he database
-
-    if not existing_post:
+    post = post_query.first()
+    if post == None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found"
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} does not exist",
         )
 
-    post_query.update(post.model_dump(), synchronize_session=False)
+    if post.owner_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to perform requested action",
+        )
+
+    post_query.update(updated_post.model_dump(), synchronize_session=False)
 
     db.commit()
 
-    return {"data": post_query.first()}
+    return post_query.first()
