0c6e6cdf8088f18c815eec86e128ded3d8dd4fd2
aashutosh-yadav <yadavashutoshh01@gmail.com>
2026-05-23 00:20:05 +0530

    config for .env

--- app/config.py
+++ app/config.py
@@ -1,16 +1,18 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings  import BaseSettings
+
 
 class Settings(BaseSettings):
-    database_hostname : str = "localhost"
-    database_port     : str = "5432"
-    database_password : str
-    database_name     : str
-    database_username : str = "postgres"
-    secret_key        : str
-    algorithm         : str = "HS256"
-    access_token_expire_minutes: int = 60
+    database_hostname: str = "localhost"      # optional in .env
+    database_port: str = "5432"               # optional in .env
+    database_password: str                    # required in .env
+    database_name: str                        # required in .env
+    database_username: str = "postgres"       # optional in .env
+    secret_key: str                           # required in .env
+    algorithm: str = "HS256"                  # optional in .env
+    access_token_expire_minutes: int = 30     # optional in .env
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
